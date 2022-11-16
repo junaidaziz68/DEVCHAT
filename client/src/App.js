8aafcee0e15b759685cdadef39583a9e47bbbf3f
junaidaziz68 <junaidaziz68@outlook.com>
2022-11-16 13:58:47 +0000

    updated files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React,{Fragment} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Navbar from './layout/Navbar';
+import { Landing } from './layout/Landing';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<Router>
+<Fragment>
+<Navbar />
+<Route path='/' element ={<Landing />} />
+</Fragment>
+</Router>
+
+    
   );
 }
 
